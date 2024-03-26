f1e2b3973f721051b80c277840e04751b9869d28
Marceillo De Wet <marceillo@hotmail.com>
2024-03-26 10:19:25 +0000

    Add button function in js with check and delet buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 const inputlist = document.querySelector("#inputfield");
 //note test if class name works for button
-const inputbutton = document.querySelector(".fa-solid");
+const inputbutton = document.querySelector(".listbutton");
 //targets the div ul list 
 const inputoutlist = document.querySelector(".listblocks");
 const filterview = document.querySelector(".filter");
@@ -11,7 +11,7 @@ const filterview = document.querySelector(".filter");
 
 //lisener
 // eventlisner to get the document to local storage 
-inputbutton.addEventListener()
+inputbutton.addEventListener("click", taskAdd);
 
 
 
@@ -21,4 +21,29 @@ inputbutton.addEventListener()
 
 //functions 
 
-function 
+function taskAdd(event) {
+    //prevent form from submitting 
+    event.preventDefault();
+    //outlist div 
+   const outDiv = document.createElement("div");
+   outDiv.classList.add("outdivcontainer");
+   //create Li 
+   const newListItem = document.createElement('li');
+   newListItem.innerText = 'mmm';
+   newListItem.classList.add('new-item');
+   outDiv.appendChild(newListItem);
+   //check button 
+   const checkButton = document.createElement('button');
+   checkButton.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+   checkButton.classList.add("done");
+   outDiv.appendChild(checkButton);
+   //delete Button 
+   const deletButton = document.createElement('button');
+   deletButton.innerHTML = '<i class="fa-solid fa-square-minus"></i>';
+   deletButton.classList.add("done");
+   outDiv.appendChild(deletButton);
+   //add to ul list 
+   inputoutlist.appendChild(outDiv);
+
+   
+}
