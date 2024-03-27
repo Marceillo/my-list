15516d471b8bbcbb7fc42e9e4c7dee61d0cb951e
Marceillo De Wet <marceillo@hotmail.com>
2024-03-27 11:12:28 +0000

    Add function delet and check button to js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const inputlist = document.querySelector("#inputfield");
 const inputbutton = document.querySelector(".listbutton");
 //targets the div ul list 
 const inputoutlist = document.querySelector(".listblocks");
+//To filter the view 
 const filterview = document.querySelector(".filter");
 
 
@@ -12,11 +13,7 @@ const filterview = document.querySelector(".filter");
 //lisener
 // eventlisner to get the document to local storage 
 inputbutton.addEventListener("click", taskAdd);
-
-
-
-
-
+inputoutlist.addEventListener('click', deletDone)
 
 
 //functions 
@@ -24,27 +21,55 @@ inputbutton.addEventListener("click", taskAdd);
 function taskAdd(event) {
     //prevent form from submitting 
     event.preventDefault();
-    
+
     //outlist div 
-   const outDiv = document.createElement("div");
-   outDiv.classList.add("listdiv");
-   //create Li 
-   const newListItem = document.createElement('li');
-   newListItem.innerText = inputlist.value;
-   newListItem.classList.add('new-item');
-   outDiv.appendChild(newListItem);
-   //check button 
-   const checkButton = document.createElement('button');
-   checkButton.innerHTML = '<i class="fa-solid fa-square-check"></i>';
-   checkButton.classList.add("done");
-   outDiv.appendChild(checkButton);
-   //delete Button 
-   const deletButton = document.createElement('button');
-   deletButton.innerHTML = '<i class="fa-solid fa-square-minus"></i>';
-   deletButton.classList.add("delet");
-   outDiv.appendChild(deletButton);
-   //add to ul list 
-   inputoutlist.appendChild(outDiv);
- 
+    const outDiv = document.createElement("div");
+    outDiv.classList.add("listdiv");
    
+    //create Li 
+    const newListItem = document.createElement('li');
+    newListItem.innerText = inputlist.value;
+    newListItem.classList.add('new-item');
+    outDiv.appendChild(newListItem);
+   
+    //check button 
+    const checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+    checkButton.classList.add("done");
+
+    //checkbutton when clicked 
+    checkButton.addEventListener('click', event => {
+        const parent = event.currentTarget.parentElement;
+        if (parent.style.textDecoration === 'line-through') {
+            parent.style.textDecoration = 'none';
+            parent.style.removeProperty('textDecorationThickness');
+        } else {
+            parent.style.textDecoration = 'line-through';
+        }
+    });
+
+    outDiv.appendChild(checkButton);
+
+    //delete Button 
+    const deletButton = document.createElement('button');
+    deletButton.innerHTML = '<i class="fa-solid fa-square-minus"></i>';
+    deletButton.classList.add("delet");
+    //delete button when clicked
+    deletButton.addEventListener('click', event => {
+        const parent = event.currentTarget.parentElement;
+        parent.remove()
+    });
+
+    outDiv.appendChild(deletButton);
+    //add to ul list 
+    inputoutlist.appendChild(outDiv);
+    //clear input 
+    inputlist.value = "";
+    // add to local storage 
+
 }
+
+//Seleect Menu button 
+
+
+
